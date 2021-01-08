3427b13ac998b60b8b5d5d999b10c5565043b3b0
CarlosChiarelli <ca.chiarelli.97@gmail.com>
2021-01-07 23:13:05 -0300

    alteracao view

--- igti/03_modulo/filmes_django/auctusflixweb/principal/views.py
+++ igti/03_modulo/filmes_django/auctusflixweb/principal/views.py
@@ -3,13 +3,14 @@
 A função "render" renderiza o html.
 "HttpResponse" será para testar a visualização.
 """
-
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    """Cria a visualização nomeada em principal/urls ("index")."""
-    return HttpResponse("Olá mundo!")
+    """Cria a visualização nomeada em principal/urls ("index").
+
+    Esse index deve mostrar/renderizar o templates criado.
+    """
+    return render(request, 'principal/index.html')
